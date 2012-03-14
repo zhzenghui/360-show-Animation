4d852e89346bfdb35c8137dfd92dd85e4c002cc0
dong xin <dongxin@localhost.(none)>
2012-03-14 11:22:28 +0800

    first commit

---
+++ Animation/Animation/DRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DRAppDelegate.h
+//  Animation
+//
+//  Created by dong xin on 12-3-14.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
